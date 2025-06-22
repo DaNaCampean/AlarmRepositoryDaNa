8ae302d14afee283d8ddb7c3303f24eb8c315edf
DaNa Campean <campean.dana@gmail.com>
2025-06-22 23:55:18 +0300

    HideHeroCard - in progress info questions - updated

--- src/tests/heroCard/InfoQuestions.java
+++ src/tests/heroCard/InfoQuestions.java
@@ -10,9 +10,15 @@ Test@123456
 1. aveti suitele cu baseTestul lor?
 2. init driver...va fi duplicat
 3. preconditiile? vor fi in baseTest?
+
 4. pot da play and stop...dar nu reusesc verificare play sau paused..
 am verificat cu ca exista play button sau nu...dar evident imi da warniung ca nu gaseste xpath de buton...
 am folosit si hidden prop la butonul de play...dar same stufff nu gasesste xpathul
+
+daca e video sau nu e diferit. initial la video am facut cu prop pause...
+dar nu merge la arm disarm...
+finalul e cu percentage...pare sa fie ok la ambele
+
 
 5. verificare gif ...am pus in hero... oare trebuie in basePage? din start? ca poate vor mai fi pe proiect verificrai?
 sau intai se pune in hero...si cand mai are ceva la fel se muta in base?
